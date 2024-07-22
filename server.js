2bc32dae17e373800de179e96eba07acd9506f8a
anrichter <anrichter2@gmail.com>
2024-07-21 20:40:25 -0500

    cleaned up server.js file. Added more code to cli.js file but experiencing multiple bugs.

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-// const express = require('express');
-// const app = express();
 const { Pool } = require('pg');
 require('dotenv').config();
 const CLI = require('./lib/cli.js');
 
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
 const pool = new Pool(
     {
-        id: process.env.DB_USERNAME,
+        user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         database: process.env.DB_DATABASE
@@ -24,11 +17,3 @@ pool.connect();
 const cli = new CLI();
 
 cli.run();
-
-// app.use((req, res) => {
-//     res.status(404).end();
-//   });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
